9f6c4c27e89ea87ae1a04ed1841b0d45650f472c
Julia Cooper <juliacooper@Julias-MacBook-Pro-6.local>
2017-12-03 22:07:14 -0500

    Commented music Generator class

--- musicGenerator.py
+++ musicGenerator.py
@@ -1,4 +1,5 @@
-#musicGenerator.py
+# musicGenerator.py
+# Contains class definition of musicGenerator
 #! /usr/bin/env python
 
 import sys
@@ -13,6 +14,9 @@ shortsilence = pyglet.media.load('hp_music/silence_1.wav', streaming=False)
 
 class musicGenerator:
     def __init__(self, data, maxTime, charFile, toneFile):
+        """Initialize an instance of the musicGenerator class, given a
+        dictionary of tone/character data for each paragraph, a max amount of
+        time, and 2 files containing lists of chars/tones and their music"""
         self.PTable = data
         self.tableLen = len(self.PTable)
         self.charDict = self.unpackFile(charFile)
@@ -22,6 +26,9 @@ class musicGenerator:
         self.maxTime = maxTime
 
     def unpackFile (self, fname):
+        """Given the name of a file, unpack the characters or tones inside the
+        file and their corresponding music pieces into a dictionary to be
+        returned"""
         newDict = {}
         with open(fname,'r') as f:
             for line in f:
@@ -31,9 +38,13 @@ class musicGenerator:
         return newDict
 
     def start(self):
+        """Start tone and character threads to read music data and queue music
+        to play in an order, then play all resulting queues simultaneously"""
         threads = []
+        # set up tone thread
         toneThread = Thread(target=self.playTone)
         threads.append(toneThread)
+        # set up character threads
         for charName in self.charDict:
             newThread = Thread(target=playChar, args=(charName,))
             threads.append(newThread)
@@ -41,39 +52,55 @@ class musicGenerator:
             t.start()
         for t in threads:
             t.join()
+        # play all players at the same time
         pgroup = pyglet.media.PlayerGroup(self.players)
         pgroup.play()
+        # halt music playing after maxTime is reached
         pyglet.clock.schedule_once(self.exit_callback, self.maxTime)
         pyglet.app.run()
 
     def exit_callback(dt):
+        """ Exit the pyglet app """
         pyglet.app.exit()
 
     def playTone(self):
+        """ For each paragraph in the table, queue appropriate music for the
+        tone to a pyglet player"""
+        player
         newplayer = pyglet.media.Player()
+        # loop through every paragraph in the table, in order
         for p in range (1, self.tableLen + 1):
             pInfo = self.PTable[p]
             tone = pInfo["tone"]
             newplayer.queue(self.toneDict[tone])
+            # small break before next paragraph
             newplayer.queue(shortsilence)
+        # add player to shared player list
         self.playerLock.acquire()
         self.players.append(newplayer)
         self.playerLock.release()
 
     def playChar(self, charName):
+        """For each paragraph in the table, queue the music for the given
+        character to a pyglet player if they appear"""
         newplayer = pyglet.media.Player()
+        # loop through every paragraph in table
         for p in range (1, self.tableLen + 1):
             pInfo = self.PTable[p]
             charList = pInfo["characters"]
             nameFound = False
+            # search for character in charList for paragraph
             for aName in charList:
                 if aName == charName:
                     nameFound = True
                     newplayer.queue(self.charDict[aName])
                     break;
+            # queue silence if not found
             if not nameFound:
                 newplayer.queue(longsilence)
+            # small break before next paragraph
             newplayer.queue(shortsilence)
+        # add player to shared player list
         self.playerLock.acquire()
         self.players.append(newplayer)
         self.playerLock.release()
